703d7847cc686224776b59f3d34980f2c2886190
Charlie Bennett <88194677+Chazit@users.noreply.github.com>
2021-08-14 01:51:26 +0100

    Changed losing loops to keep number consistent

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ print(artwork.title)
 
 streak = 0
 end_of_game = False
-first_number = random.randint(1, 21)
+first_number = 11
 
 print("Welcome to High or Low!\nDo you think the next number will be higher or lower?")
-print(f"\nThe game always starts with the number {first_number}, get a streak of 5 to win!")
+print(f"\nThe game always starts with the number 11, get a streak of 5 to win!")
 
 while not end_of_game:
     second_number = random.randint(1, 21)
@@ -27,9 +27,13 @@ while not end_of_game:
     elif player_input == "l" and second_number > first_number:
         print(f"{second_number}, Unlucky")
         streak = 0
+        first_number = second_number
+        print(f"\n\nThe current number is: {first_number}")
     elif player_input == "h" and second_number < first_number:
         print(f"{second_number}, Unlucky")
         streak = 0
+        first_number = second_number
+        print(f"\n\nThe current number is: {first_number}")
     elif player_input == "l" and second_number < first_number:
         streak += 1
         print(f"{second_number} Correct!")
